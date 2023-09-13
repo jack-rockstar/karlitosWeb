db2ad91ddaeb0f060fb71d2170f898383f9eb2dc
Carlos Colina <carlosecolina89@gmail.com>
2023-09-13 18:17:38 -0500

    actualizacion del body

--- src/app/components/Body/Body.jsx
+++ src/app/components/Body/Body.jsx
@@ -1,5 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function Body() {
+  const mainStyle = {
+    background: 'url("./img/auto.png")', // Reemplaza con la ruta de tu imagen de fondo
+    backgroundSize: 'cover' // Esto asegura que la imagen se ajuste al tamaño del contenedor
+  }
+
+  const estiloPersonalizado = {
+    color: '#444444',
+    fontSize: '26px',
+  }
+  const h1Style = {
+    color: '#FF0082',
+    fontSize: '30px',
+  }
+
   return (
-    <div className='h-screen'>Body</div>
+    <main className='h-screen bg ' style={mainStyle}>
+
+      <div className='max-w-5xl pt-20'>
+        <div className='row grid sm:grid-cols-6 md:grid-cols-12 justify-center '>
+          <div className=' col-span-6   mb-4 col p-4'>
+            <div className='rounded p-1 bg-white'>
+              <ul className='steps mb-0'>
+                <li className='active'>
+                  <a href='#'>
+                    <i>
+                      
+                    </i> fdsdfsdf
+                  </a>
+                </li>
+                <li></li>
+                <li></li>
+              </ul>
+              <form action=""></form>
+            </div>
+          </div>
+          <div className='= col-span-6  mb-4 block-side-form col p-4'><h1 style={h1Style}>Cotizador de seguro para Autos</h1> <h2 style={estiloPersonalizado}>Obtené hasta un 30% de descuento en los mejores seguros de autos</h2></div>
+        </div>
+      </div>
+
+    </main>
   )
 }
--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ export default function Navbar() {
     <nav className='border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700'>
       <div className='flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto'>
         <a href='#' className='flex items-center'>
-          <img src='https://flowbite.com/docs/images/logo.svg' className='h-8 mr-3' alt='Flowbite Logo' />
-          <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>Flowbite</span>
+          <img src='https://34.95.113.167/dist/img/logoavatar.png' className='h-8 mr-3' alt='Karlitos Web Logo' />
+          <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>Karlitos Seguros </span>
         </a>
         <button data-collapse-toggle='navbar-solid-bg' type='button' className='inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600' aria-controls='navbar-solid-bg' aria-expanded='false'>
           <span className='sr-only'>Open main menu</span>
@@ -16,17 +16,17 @@ export default function Navbar() {
         <div className='hidden w-full md:block md:w-auto' id='navbar-solid-bg'>
           <ul className='flex flex-col mt-4 font-medium rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700'>
             <li>
-              <a href='#' className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent' aria-current='page'>Home</a>
+              <a href='#' className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent' aria-current='page'>Autos</a>
             </li>
-            <li>
+            {/* <li>
               <a href='#' className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'>Services</a>
-            </li>
-            <li>
+            </li> */}
+            {/* <li>
               <a href='#' className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'>Pricing</a>
-            </li>
-            <li>
+            </li> */}
+            {/* <li>
               <a href='#' className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'>Contact</a>
-            </li>
+            </li> */}
           </ul>
         </div>
       </div>
