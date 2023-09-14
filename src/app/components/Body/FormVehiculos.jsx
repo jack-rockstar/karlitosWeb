8ad90893f9661e1c654864273e8cb69a4fa23624
Carlos Colina <carlosecolina89@gmail.com>
2023-09-14 10:43:55 -0500

    cambios stylo formulario

--- src/app/components/Body/FormVehiculos.jsx
+++ src/app/components/Body/FormVehiculos.jsx
@@ -27,9 +27,9 @@ export default function FormVehiculos() {
 
   return (
     <div>
-      <ul className='mb-0 grid   md:grid-cols-3'>
+      <ul className='mb-0 grid   md:grid-cols-3  '>
         <li className='active text-center bg-orange-600 p-2'>
-          <a href='#'>
+          <a href='#' className='  text-white'>
             Primer enlace
 
           </a>
@@ -45,11 +45,10 @@ export default function FormVehiculos() {
           </a>
         </li>
       </ul>
-      <div>
-        <div>
-          <label>Dropdown 1:</label>
-          <select value={selectedOption1} onChange={handleOption1Change}>
-            <option value="">Selecciona una opción</option>
+      <div className='p-4 grid   '>
+        <div className=' grid grid-cols-1 mb-2 rounded  border-2'>
+          <select value={selectedOption1} onChange={handleOption1Change} className='rounded'>
+            <option value=''>Marca</option>
             {opciones1.map((opcion) => (
               <option key={opcion} value={opcion}>
                 {opcion}
@@ -58,14 +57,14 @@ export default function FormVehiculos() {
           </select>
         </div>
 
-        <div>
-          <label>Dropdown 2:</label>
+        <div className=' grid grid-cols-1 mb-2 rounded  border-2'>
           <select
             value={selectedOption2}
             onChange={handleOption2Change}
             disabled={!selectedOption1}
+            className='rounded'
           >
-            <option value="">Selecciona una opción</option>
+            <option value=''>Modelo/Version </option>
             {opciones2.map((opcion) => (
               <option key={opcion} value={opcion}>
                 {opcion}
@@ -74,20 +73,34 @@ export default function FormVehiculos() {
           </select>
         </div>
 
-        <div>
-          <label>Dropdown 3:</label>
+        <div className=' grid grid-cols-1 mb-2 rounded  border-2'>
           <select
             value={selectedOption3}
             onChange={handleOption3Change}
             disabled={!selectedOption2}
+            className='rounded'
           >
-            <option value="">Selecciona una opción</option>
+            <option value=''>Año</option>
             {opciones3.map((opcion) => (
               <option key={opcion} value={opcion}>
                 {opcion}
               </option>
             ))}
           </select>
+        </div>
+        <div className='grid grid-cols-3'>
+          <div></div>
+          <div className='text-right'><label htmlFor=''>Es 0 Km?:</label></div>
+          <div className='grid grid-cols-4 text-center'>
+            Si
+            <input id='default-radio-1' type='radio' value='' name='default-radio' className='w-4  text-blue-600 bg-gray-100 border-gray-300  dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'></input>
+            No
+            <input checked id="default-radio-2" type="radio" value="" name="default-radio" className="w-4  text-blue-600 bg-gray-100 border-gray-300  dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+          </div>
+        </div>
+        <div className='grid grid-cols-1'>
+          <button type='button' className='' ></button>
+          
         </div>
       </div>
     </div>
