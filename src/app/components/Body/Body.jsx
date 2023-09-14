d4bff316d8fdf2e663a1aa7826bdc73086b5d211
Carlos Colina <carlosecolina89@gmail.com>
2023-09-14 08:01:40 -0500

    cambios body elemnts

--- src/app/components/Body/Body.jsx
+++ src/app/components/Body/Body.jsx
@@ -5,12 +5,12 @@ export default function Body() {
   return (
     <Container>
       <div className='bg-cover bg-image-main min-h-[560px] grid place-content-center'>
-        <article className='grid justify-center px-4 max-w-7xl row sm:grid-cols-6 md:grid-cols-12'>
+        <article className='grid justify-center px-6 max-w-6xl row sm:grid-cols-6 md:grid-cols-12'>
           <section className='col-span-6 p-1 mb-4 bg-white rounded'>
             <ul className='mb-0 steps'>
               <li className='active'>
                 <a href='#'>
-                  <i /> fdsdfsdf
+
                 </a>
               </li>
               <li />
@@ -18,7 +18,7 @@ export default function Body() {
             </ul>
             <form action='' />
           </section>
-          <section className='col-span-6 p-4 mb-4'>
+          <section className='col-span-6 p-6 pt-0 mb-4'>
             <h1 className='text-3xl font-bold text-primary'>Cotizador de seguro para Autos</h1>
             <span className='text-2xl text-black/70'>Obtené hasta un 30% de descuento en los mejores seguros de autos</span>
           </section>
