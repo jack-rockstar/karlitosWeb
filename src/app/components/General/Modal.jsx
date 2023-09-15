a8ce0a274e7acd57ddd013870b73243f6f533f57
Jack Najarro <jacknajarro1@gmail.com>
2023-09-15 11:27:08 -0500

    nt

--- src/app/components/General/Modal.jsx
+++ src/app/components/General/Modal.jsx
@@ -6,7 +6,7 @@ import Portal from './Portal'
 export default function Modal({ children, width = 750, isOpen, closeModal }) {
   return (
     <Portal>
-      <div className={`${isOpen ? 'hidden' : 'fixed overflow-hidden grid justify-center items-center bottom-0 top-0 py-4 left-0 bg-black/80 right-0 z-50 w-full p-4  md:inset-0 min-h-screen h-fit '}`}>
+      <div className={`${!isOpen ? 'hidden' : 'fixed overflow-hidden grid justify-center items-center bottom-0 top-0 py-4 left-0 bg-black/80 right-0 z-50 w-full p-4  md:inset-0 min-h-screen h-fit '}`}>
         <div className='relative w-full'>
           <div className={`relative bg-white rounded-lg shadow w-[${String(width)}px] my-8`}>
             <Button type='button' onClick={() => closeModal()}>
