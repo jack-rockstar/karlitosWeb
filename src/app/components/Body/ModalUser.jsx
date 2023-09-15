11f5b6a80ba45c60f53a571968894d28e2adb994
Carlos Colina <carlosecolina89@gmail.com>
2023-09-15 11:30:30 -0500

    cambios componentes modal

--- src/app/components/Body/ModalUser.jsx
+++ src/app/components/Body/ModalUser.jsx
@@ -43,42 +43,35 @@ function ModalUser({ openModal, closeModal }) {
             </div>
             <div className='p-6 space-y-6'>
               <div className='grid grid-cols-2'>
-                <div className='relative z-0'>
+                <div className='relative z-0 p-4'>
                   <input type='text' id='floating_outlined' className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' placeholder=' ' />
                   <label for='floating_outlined' className='absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Nombres</label>
                 </div>
-                <div className='relative z-0'>
+                <div className='relative z-0 p-4'>
                   <input type='text' id='floating_outlined' className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' placeholder=' ' />
                   <label for='floating_outlined' className='absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Apellidos</label>
                 </div>
               </div>
-              <div className='grid grid-cols-1'>
+              <div className='grid grid-cols-1 p-4'>
                 <div className='relative z-0'>
                   <input type='text' id='floating_outlined' className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' placeholder=' ' />
                   <label for='floating_outlined' className='absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Email</label>
                 </div>
               </div>
-              <div className='grid grid-cols-1'>
+              <div className='grid grid-cols-1 p-4'>
                 <div className='relative z-0'>
                   <input type='text' id='floating_outlined' className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' placeholder=' ' />
                   <label for='floating_outlined' className='absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Telefono</label>
                 </div>
               </div>
             </div>
-            <div className='flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600'>
+            <div className='flex items-center  justify-center  p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600'>
               <button
                 onClick={closeModal}
                 type='button'
-                className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                className='shadow-md bg-gradient-to-b from-orange-500 to-yellow-300 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
               >
-                I accept
-              </button>
-              <button
-                onClick={closeModal}
-                type='button'
-                className='text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600'
-              >
-                Decline
+                Ver la cotización
               </button>
             </div>
           </div>
