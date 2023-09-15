39e43af99b2d6d289ccd6125a1f55ff07d6271de
Carlos Colina <carlosecolina89@gmail.com>
2023-09-15 17:01:30 -0500

    cambios modal user

--- src/app/components/Body/ModalUser.jsx
+++ src/app/components/Body/ModalUser.jsx
@@ -1,37 +1,85 @@
+'use client'
 import Modal from '@components/General/Modal'
-import React from 'react'
+import React, { useState } from 'react'
+import InputLabel from '@components/General/InputLabel'
 
 function ModalUser({ isModalOpenUser, closeModalUser }) {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+  const [tipoDoc, setTipoDoc] = useState('Tipo de Doc.')
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen)
+  }
+  const changeTipeDoc = (e) => {
+    setTipoDoc(e.target.value)
+    toggleDropdown()
+  }
+
   return (
     <Modal closeModal={closeModalUser} width={500} isOpen={isModalOpenUser}>
       <div className='relative w-full max-w-2xl max-h-full'>
         <div className='relative  '>
           <div className='flex items-start justify-between p-4 '>
-            <h3 className='text-xl font-semibold text-gray-900 '>
+            <h3 className='text-xl font-semibold  text-blue-950'>
               Necesitamos unos datos más...
             </h3>
           </div>
           <div className='p-6 space-y-6'>
             <div className='grid grid-cols-2'>
-              <div className='relative z-0 p-4'>
-                <input type='text' id='floating_outlined' className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' placeholder=' ' />
-                <label for='floating_outlined' className='absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Nombres</label>
+              <div className='relative  p-1'>
+                <button
+                  id='dropdownHoverButton' data-dropdown-toggle='dropdownHover' data-dropdown-trigger='hover'
+                  onClick={toggleDropdown}
+                  className=' justify-between  px-5 py-2 w-full text-sm text-gray-900 bg-transparent rounded-lg border-2  appearance-none  border-blue-500 focus:border-blue-500 focus:outline-none focus:ring-0 peer inline-flex text-center' type='button'
+                >
+                  {tipoDoc}
+                  <svg className=' w-2.5 h-2.5 ml-2.5 right-2 text-center' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 10 6'>
+                    <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='m1 1 4 4 4-4' />
+                  </svg>
+                </button>
+                {isDropdownOpen && (
+                  <div className='origin-top-right absolute z-10
+                      mt-2 w-44 rounded-lg shadow-lg bg-white divide-y divide-gray-100 dark:bg-gray-700'>
+                    <ul className='py-2 text-sm text-gray-700 dark:text-gray-200 z-0'>
+                      <li>
+                        <a onClick={changeTipeDoc} href='#' className=' px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
+                          DNI
+                        </a>
+                      </li>
+                      <li>
+                        <a onClick={changeTipeDoc} href='#' className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
+                          RUC
+                        </a>
+                      </li>
+                      <li>
+                        <a onClick={changeTipeDoc} href='#' className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
+                          CE
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                )}
               </div>
-              <div className='relative z-0 p-4'>
-                <input type='text' id='floating_outlined' className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' placeholder=' ' />
-                <label for='floating_outlined' className='absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Apellidos</label>
+              <div className='relative z-0 p-1'>
+                <InputLabel label='N Documento' />
               </div>
             </div>
-            <div className='grid grid-cols-1 p-4'>
-              <div className='relative z-0'>
-                <input type='text' id='floating_outlined' className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' placeholder=' ' />
-                <label for='floating_outlined' className='absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Email</label>
+            <div className='grid grid-cols-2'>
+              <div className='relative z-0 p-1'>
+                <InputLabel label='Nombres' />
+              </div>
+              <div className='relative z-0 p-1'>
+                <InputLabel label='Apellidos' />
               </div>
             </div>
-            <div className='grid grid-cols-1 p-4'>
+            <div className='grid grid-cols-1 p-1'>
               <div className='relative z-0'>
-                <input type='text' id='floating_outlined' className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' placeholder=' ' />
-                <label for='floating_outlined' className='absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Telefono</label>
+                <InputLabel label='Email' />
+              </div>
+            </div>
+            <div className='grid grid-cols-1 p-1'>
+              <div className='relative z-0 '>
+                <InputLabel label='Telefono' />
               </div>
             </div>
           </div>
@@ -39,12 +87,29 @@ function ModalUser({ isModalOpenUser, closeModalUser }) {
             <button
               onClick={closeModalUser}
               type='button'
-              className='shadow-md bg-gradient-to-b from-orange-500 to-yellow-300 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+              className='shadow-md  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+              style={{ background: 'linear-gradient(87deg,#f97316 0%,#af09d8   100%)' }}
             >
               Ver la cotización
             </button>
           </div>
         </div>
+      </div>
+      <div id='dropdownHover' className='z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700'>
+        <ul className='py-2 text-sm text-gray-700 dark:text-gray-200' aria-labelledby='dropdownHoverButton'>
+          <li>
+            <a href='#' className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Dashboard</a>
+          </li>
+          <li>
+            <a href='#' className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Settings</a>
+          </li>
+          <li>
+            <a href='#' className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Earnings</a>
+          </li>
+          <li>
+            <a href='#' className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Sign out</a>
+          </li>
+        </ul>
       </div>
     </Modal>
   )
